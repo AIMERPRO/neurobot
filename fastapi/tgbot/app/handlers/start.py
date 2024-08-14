066500c047e4f8904c18f178e19b63754dd9fd01
AIMERPRO <potrebchuk0@gmail.com>
2024-08-14 20:10:27 +0300

    fix

--- fastapi/tgbot/app/handlers/start.py
+++ fastapi/tgbot/app/handlers/start.py
@@ -20,21 +20,19 @@ from loguru import logger
 start_router = Router()
 
 
-
 @start_router.message(Command("start"))
 @inject
 async def start_handler(msg: Message):
-    user = await User.query.where(User.chat_id == str(msg.from_user.id)).gino.first()
+    chat_id = msg.from_user.id
+    user = await User.query.where(User.chat_id == str(chat_id)).gino.first()
+    if user:
+        pass
+    else:
+        await User.create(name=msg.from_user.full_name, username=msg.from_user.username, chat_id=str(chat_id))
 
     if user.subscribe_end <= datetime.datetime.now():
-        chat_id = msg.from_user.id
-        user = await User.query.where(User.chat_id == str(chat_id)).gino.first()
-        if user:
-            pass
-        else:
-            await User.create(name=msg.from_user.full_name, username=msg.from_user.username, chat_id=str(chat_id))
-
         await msg.answer("Оплатите подписку /pay")
 
     else:
-        await msg.answer(f"Ваша подписка оплачена до {datetime.datetime.strftime(user.subscribe_end, "%d-%m-%Y, %H-%S")}")
+        await msg.answer(f"Ваша подписка действительна до: {datetime.datetime.strftime(user.subscribe_end, '%d-%m-%Y, %H-%S')}")
+
